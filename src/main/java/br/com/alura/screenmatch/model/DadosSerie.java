fb426ca63feebe6fe3c10dafbbe28df926ae696e
Jefferson Silva <jefferson.silva.jps@gmail.com>
2024-02-07 21:38:18 -0300

    Inicio do projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,15 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+//ignora as propriedades que não foram solicitadas.
+
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
+
+    //Cria um apelido para os dados @JsonAlias
+
+}
